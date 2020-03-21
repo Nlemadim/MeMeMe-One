8b8261fe4d1daaf4d5820f5f661526ef2af4798b
Tony Nlemadim <nlemadimtony@gmail.com>
2020-03-21 05:25:26 -0400

    Initial Commit

---
+++ MeMeMe One/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeMeMe One
+//
+//  Created by Tony Nlemadim on 3/21/20.
+//  Copyright © 2020 Tonys Dev Ops. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
